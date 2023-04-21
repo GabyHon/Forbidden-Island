17bd49df3b111de96ceec92914b5df0188dc2a19
Gaby <129265520+GabyHon@users.noreply.github.com>
2023-04-21 12:27:17 +0200

    Level-Selector

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelector : MonoBehaviour
+{
+    [SerializeField] private int difficulty = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (difficulty)
+        {
+            case 1:
+                print("Novice level was chosen");
+                break;
+            case 2:
+                print("Normal level was chosen");
+                break;
+            case 3:
+                print("Elite level was chosen");
+                break;
+            case 4:
+                print("Legendary level was chosen");
+                break;
+            default:
+                print("Please choose a level");
+                break;
+        }
+    }
+}
