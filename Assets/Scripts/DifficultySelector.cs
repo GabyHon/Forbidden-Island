9ff1d7b95907266cc3bb8bc19f82e80c9c860944
Gaby <129265520+GabyHon@users.noreply.github.com>
2023-05-04 12:11:27 +0200

    Bool check added
    Added a bool check for the difficulty and player number to appear in the inspector.

--- Assets/Scripts/DifficultySelector.cs
+++ Assets/Scripts/DifficultySelector.cs
@@ -11,24 +11,48 @@ public class DifficultySelector : MonoBehaviour
 
     public DifficultyChosen difficulty;
 
-    // Update is called once per frame
+    public bool Novice = false;
+    public bool Normal = false;
+    public bool Elite = false;
+    public bool Legendary = false;
+
     void Update()
     {
         switch (difficulty)
         {
             case DifficultyChosen.Select:
+                Novice = false;
+                Normal = false;
+                Elite = false;
+                Legendary = false;
                 print("Please select a difficulty");
                 break;
             case DifficultyChosen.Novice:
+                Novice = true;
+                Normal = false;
+                Elite = false;
+                Legendary = false;
                 print("Novice was chosen");
                 break;
             case DifficultyChosen.Normal:
+                Novice = false;
+                Normal = true;
+                Elite = false;
+                Legendary = false;
                 print("Normal was chosen");
                 break;
             case DifficultyChosen.Elite:
+                Novice = false;
+                Normal = false;
+                Elite = true;
+                Legendary = false;
                 print("Elite was chosen");
                 break;
             case DifficultyChosen.Legenary:
+                Novice = false;
+                Normal = false;
+                Elite = false;
+                Legendary = true;
                 print("Legendary was chosen");
                 break;
         }
--- Assets/Scripts/playerNumbers.cs
+++ Assets/Scripts/playerNumbers.cs
@@ -11,21 +11,36 @@ public class playerNumbers : MonoBehaviour
 
     public HowManyPlayers players;
 
-    // Update is called once per frame
+    public bool Two = false;
+    public bool Three = false;
+    public bool Four = false;
+
     void Update()
     {
         switch (players)
         {
             case HowManyPlayers.Select:
+                Two = false;
+                Three = false;
+                Four = false;
                 print("Please select the number of players");
                 break;
             case HowManyPlayers.Two:
+                Two = true;
+                Three = false;
+                Four = false;
                 print("2 Players");
                 break;
             case HowManyPlayers.Three:
+                Two = false;
+                Three = true;
+                Four = false;
                 print("3 Players");
                 break;
             case HowManyPlayers.Four:
+                Two = false;
+                Three = false;
+                Four = true;
                 print("4 Players");
                 break;
         }
