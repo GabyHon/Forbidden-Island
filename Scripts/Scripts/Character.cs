906bdc26f420e8515a28faf433dfe6f9040fec12
Mpumiii <130597279+Mpumiii@users.noreply.github.com>
2023-06-20 22:08:34 +0200

    Update Character.cs

--- Scripts/Scripts/Character.cs
+++ Scripts/Scripts/Character.cs
@@ -1,19 +1,14 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 using UnityEngine.UI;
 
 public class Character : MonoBehaviour
 {
-
     public List<SpecialAbility> specialAbilities;
-    public Button ShoreUp;
-    public Button moveOtherPlayers;
+    public Button shoreUpButton;
+    public Button moveOtherPlayersButton;
 
     // Add any additional character-specific properties or methods here
-
-
 
     public enum SpecialAbility
     {
@@ -23,8 +18,6 @@ public class Character : MonoBehaviour
         Messenger,
         Diver,
         Explorer,
-
-
     }
 
     public void PerformAbility(int abilityIndex)
@@ -36,30 +29,29 @@ public class Character : MonoBehaviour
             switch (ability)
             {
                 case SpecialAbility.Engineer:
-                    ShoreUp.gameObject.SetActive(true);
-                    moveOtherPlayers.gameObject.SetActive(false);
+                    //shoreUpButton.gameObject.SetActive(true);
+                    //moveOtherPlayersButton.gameObject.SetActive(false);
                     break;
                 case SpecialAbility.Explorer:
-                    ShoreUp.gameObject.SetActive(false);
-                    moveOtherPlayers.gameObject.SetActive(false);
+                    //shoreUpButton.gameObject.SetActive(false);
+                    //moveOtherPlayersButton.gameObject.SetActive(false);
                     break;
                 case SpecialAbility.Navigator:
-                    ShoreUp.gameObject.SetActive(false);
-                    moveOtherPlayers.gameObject.SetActive(true);
+                    //shoreUpButton.gameObject.SetActive(false);
+                    //moveOtherPlayersButton.gameObject.SetActive(true);
                     break;
                 case SpecialAbility.Pilot:
-                    ShoreUp.gameObject.SetActive(false);
-                    moveOtherPlayers.gameObject.SetActive(false);
+                    //shoreUpButton.gameObject.SetActive(false);
+                    //moveOtherPlayersButton.gameObject.SetActive(false);
                     break;
                 case SpecialAbility.Diver:
-                    ShoreUp.gameObject.SetActive(false);
-                    moveOtherPlayers.gameObject.SetActive(false);
+                    //shoreUpButton.gameObject.SetActive(false);
+                    //moveOtherPlayersButton.gameObject.SetActive(false);
                     break;
                 case SpecialAbility.Messenger:
-                    ShoreUp.gameObject.SetActive(false);
-                    moveOtherPlayers.gameObject.SetActive(false);
+                    //shoreUpButton.gameObject.SetActive(false);
+                    //moveOtherPlayersButton.gameObject.SetActive(false);
                     break;
-
                 default:
                     Debug.LogWarning("Unknown ability selected");
                     break;
@@ -69,27 +61,7 @@ public class Character : MonoBehaviour
         {
             Debug.LogError("Invalid ability index");
         }
-
     }
-
-    public Guid PlayerID;
-
-    private void Awake()
-    {
-        PlayerID = Guid.NewGuid();
-    }
-
-    public void SetPlayerID(Guid id)
-    {
-        PlayerID = id;
-    }
-
-    public Guid GetPlayerID()
-    {
-        return PlayerID;
-    }
-
-
 
 
 }
